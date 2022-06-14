b99280110d02a8af35b57c6754d561e320a8deae
Aswanth P <aswantharasu@gmail.com>
2022-06-14 18:03:03 +0530

    Spring boot application

---
+++ src/main/java/com/demo/demo/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.demo.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class TestController {
+	@GetMapping("/test")
+	public String test() {
+		return "Hello Springboot App";
+	}
+}
